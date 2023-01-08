5233908e9b3b1714500c9d09246f8023a0636ef0
klozano <klozano17@gmail.com>
2023-01-08 16:44:20 -0500

    added rest of js code and formatted

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,11 +6,11 @@ $(function () {
 
 
   var now = dayjs();
-  var civilianTime = now.format('hh');
+  var regularTime = now.format('hh');
   var militaryTime = now.format('HH');
   var currentDay = $('#currentDay');
 
-// TODO: Add a listener for click events on the save button. This code should
+  // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage.
   $('.saveBtn').click(function() {
@@ -19,42 +19,70 @@ $(function () {
     localStorage.setItem(timeslot, input);
   })
 
-//code to execute each time slot
-function setCurrentHour() {
-  $('#calendar').children('div').each(function() {
-    let divtarget = $(this);
-    let timeSlot = divtarget.attr('id');
-    let timeVal = timeSlot.charAt(5) + timeSlot.charAt(6);
-    let priorEntries = localStorage.getItem(timeSlot);
+  //code to execute each time slot
+  function setCurrentHour() {
+    $('#calendar').children('div').each(function() {
+      let divtarget = $(this);
+      let timeSlot = divtarget.attr('id');
+      let timeVal = timeSlot.charAt(5) + timeSlot.charAt(6); 
   //
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  function setToPresent() {
-    divtarget.removeClass('past future');
-    divtarget.addClass('present');
+  // block by comparing the id to the current hour. 
+      function setToPresent() {
+        divtarget.removeClass('past future');
+        divtarget.addClass('present');
+      }
+      function setToFuture() {
+        divtarget.removeClass('past present');
+        divtarget.addClass('future');
+      }
+      function setToPast() {
+        divtarget.removeClass('future present');
+        divtarget.addClass('past');
+      }
+
+  //conditional to check the times lots and compare to appropriate time 
+      if (militaryTime < 9) {
+        setToFuture();
+      } else if (militaryTime >= 18) {
+        setToPast();
+      } else if (timeVal < 9) {
+        if (timeVal == regularTime) {
+          setToPresent();
+        } else if (timeVal > regularTime || militaryTime == regularTime) {
+          setToFuture();
+        } else {
+          setToPast();
+        }
+      } else if (timeVal >= 9) {
+        if (timeVal == militaryTime) {
+          setToPresent();
+        } else if (timeVal > militaryTime) {
+          setToFuture();
+        } else {
+          setToPast();
+        }
+      }
+    })
   }
-  function setToFuture() {
-    divtarget.removeClass('past present');
-    divtarget.addClass('future');
-  }
-  function setToPast() {
-    divtarget.removeClass('future present');
-    divtarget.addClass('past');
-  }
 
 
+  //current hour function
+  setCurrentHour();
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-
-
+  setInterval(function() {
 
   // TODO: Add code to display the current date in the header of the page.
+    currentDay.text(dayjs().format('dddd, MMMM D, YYYY h:mm:ss a'));
+
+    let currentHour = dayjs().format('hh');
+    let currentHourMil = dayjs().format('HH');
+
+     if (currentHourMil != militaryTime) {
+      civilianTime = currentHour;
+      militaryTime = currentHourMil;
+      setCurrentHour();
+    }
+  }, 1000)
+
 });
