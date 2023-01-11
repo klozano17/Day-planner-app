12b8919714769d9cb26e485f28e61554929a01de
klozano <klozano17@gmail.com>
2023-01-11 12:10:17 -0500

    reverted changes to JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,11 +3,13 @@
 // in the html.
 $(function () {
 
-  var militaryTime = now.format('HH');
-  var regularTime = now.format('hh');
-  var currentDay = $('#currentDay');
+
+
   var now = dayjs();
-  
+  var regularTime = now.format('hh');
+  var militaryTime = now.format('HH');
+  var currentDay = $('#currentDay');
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage.
